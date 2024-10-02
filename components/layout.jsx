598f8dff5cb8f10fe3d96d5c4297f7bff0fb51e3
Fabian Valladolid <kadu.ochoa.27@unsch.edu.pe>
2024-10-02 18:29:31 -0500

    start

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,8 +6,8 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>IO Academy</title>
-                <meta name='description' content='Mastering computer science fundamentals from zero to hero, with the best experts around the world.' />
+                <title>Hotel_system</title>
+                <meta name='description' content='This is a description about the hotel_system' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
--- components/ui/Navbar/Navbar.jsx
+++ components/ui/Navbar/Navbar.jsx
@@ -27,9 +27,10 @@ const Navbar = () => {
     const navMenuBtnColor = addColor("text-gray-500 hover:bg-gray-50", "text-gray-400 hover:bg-gray-800")
 
     const navigation = [
-        { name: "Features", href: "/login" },
-        { name: "Lessons", href: "/tutorials/cs50" },
-        { name: "Pricing", href: "/#pricing" },
+        { name: "Nosotros", href: "/login" },
+        { name: "Habitaciones", href: "/tutorials/cs50" },
+        { name: "Reservas", href: "/#pricing" },
+        { name: "Contactanos", href: "/#pricing" },
     ]
 
     useEffect(() => {
@@ -99,7 +100,7 @@ const Navbar = () => {
                                     href="/login"
                                     className="flex items-center justify-center gap-x-1 font-medium text-sm text-white bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full"
                                 >
-                                    Sign in
+                                    Iniciar Sesion
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                                         <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
                                     </svg>
