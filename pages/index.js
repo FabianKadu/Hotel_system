9ce3da15c59e32b1700df13b6347e3915ca109eb
Fabian Valladolid <kadu.ochoa.27@unsch.edu.pe>
2024-10-02 17:58:04 -0500

    first

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Card from "../components/ui/Card";
+import Course from "../components/ui/Course";
+import CTA from "../components/ui/CTA";
+import Features from "../components/ui/Features";
+import Hero from "../components/ui/Hero";
+import Pricing from "../components/ui/Pricing";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <CTA />
+      <Features />
+      <Course />
+      <Card />
+      <Pricing />
+    </>
+  );
+}
